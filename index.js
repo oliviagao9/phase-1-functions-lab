5dc5fee00a04b025ec0b297fec8ab1a9370ca010
oliviagao9 <oliviagao9@gmail.com>
2023-08-13 11:38:49 -0500

    function lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(destination){
+    return Math.abs(destination - 42);
+}
+
+function distanceFromHqInFeet(destination){
+    return Math.abs((destination - 42) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+
+    if (distanceTravelledInFeet(start, destination) < 400) {
+
+        return 0;
+
+    } else if (distanceTravelledInFeet(start, destination) <= 2000) {
+        
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+
+    } else if (distanceTravelledInFeet (start, destination) <= 2500){
+
+        return 25;
+
+    } else return 'cannot travel that far';
+
+}
